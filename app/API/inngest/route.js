dfdf2c1bbdde8d585b63c3ff46952ccd92edbe9e
Niranjan200321 <jniranjanreddy2003@gmail.com>
2025-08-04 09:40:45 +0530

    Fix connectDB deployment error

--- app/API/inngest/route.js
+++ app/API/inngest/route.js
@@ -1,5 +1,6 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "../../../Config/inngest"; // ✅ fixed path
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "../../../Config/inngest";
+
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
